a94e068a17a404f9979a070a972b39bdb407fd13
navanee99 <navaneenagarajan305@gmail.com>
2023-07-08 14:33:58 +0530

    made common for all jobs

--- main1.py
+++ main1.py
@@ -11,18 +11,17 @@ def find_jobs():
     jobs=soup.find_all('li',class_='clearfix job-bx wht-shd-bx')
     for index,job in enumerate(jobs):
         published_date=job.find('span',class_='sim-posted').span.text
-        if('today','few' in published_date):
-            company_name=job.find('h3',class_='joblist-comp-name').text.replace(' ','')
-            job_titles=job.header.h2.a.text
-            more_info=job.header.h2.a['href']
-            skills=job.find('span',class_='srp-skills').text.replace(' ','')
-            with open(f'posts/{index}.txt','w') as f:
-                    f.write(f"Company Name:{company_name.strip()} \n")
-                    f.write(f"Job Title:{job_titles.strip()}\n")
-                    f.write(f"Required Skills:{skills.strip()} \n")
-                    f.write(f"Publised Time:{published_date.strip()}\n")
-                    f.write(f"More info:{more_info}")
-            print(f'File Saved:{index}')
+        company_name=job.find('h3',class_='joblist-comp-name').text.replace(' ','')
+        job_titles=job.header.h2.a.text
+        more_info=job.header.h2.a['href']
+        skills=job.find('span',class_='srp-skills').text.replace(' ','')
+        with open(f'posts/{index}.txt','w') as f:
+            f.write(f"Company Name:{company_name.strip()} \n")
+            f.write(f"Job Title:{job_titles.strip()}\n")
+            f.write(f"Required Skills:{skills.strip()} \n")
+            f.write(f"Publised Time:{published_date.strip()}\n")
+            f.write(f"More info:{more_info}")
+        print(f'File Saved:{index}')
 
 if __name__ == '__main__':
     while True:
